01c7c21b6d5a18ee41eddfdee752643b7e331663
Crispin Read <crispin.read@gmail.com>
2019-07-23 20:42:43 +0100

    add docs and links to footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,14 +2,15 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                <li><a href="https://twitter.com/WebDevAppNet" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                <li><a href="https://www.facebook.com/WebDevApprentice" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                <li><a href="https://www.instagram.com/webdevappnet" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="https://github.com/thecodersguilduk" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                <li><a href="https://www.linkedin.com/groups/8324305/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                <li><a href="https://www.youtube.com/channel/UC_By6GiE5aVoMyoTLr5lGZA" className="icon alt fa-youtube"><span className="label">YouTube</span></a></li>
             </ul>
             <ul className="copyright">
-                <li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+                <li>&copy; The Coders Guild 2019</li><li>Reg England and Wales: 9622444</li> <li>UKPRN 10058030</li> <li><a href="/static/docs/tcg_privacy_policy.pdf">Privacy policy</a></li> <li><a href="/static/docs/tcg_complaints_policy.pdf">Complaints policy</a></li> <li><a href="/static/docs/tcg_equality_diversity_policy.pdf">Equality and Diversity</a></li>
             </ul>
         </div>
     </footer>
