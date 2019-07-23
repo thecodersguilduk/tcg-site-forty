2bff42585efe63f4e08cddf655c56fd586c82600
Crispin Read <crispin.read@gmail.com>
2019-07-23 21:56:53 +0100

    swap links in menu

--- components/Menu.js
+++ components/Menu.js
@@ -6,13 +6,9 @@ const Menu = (props) => (
         <div className="inner">
             <ul className="links">
                 <li><Link href="/"><a><span onClick={props.onToggleMenu}>Home</span></a></Link></li>
-                <li><Link href="/landing"><a><span onClick={props.onToggleMenu}>Landing</span></a></Link></li>
-                <li><Link href="/generic"><a><span onClick={props.onToggleMenu}>Generic</span></a></Link></li>
-                <li><Link href="/elements"><a><span onClick={props.onToggleMenu}>Elements</span></a></Link></li>
-            </ul>
-            <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
-                <li><a href="#" className="button fit">Log In</a></li>
+                <li><Link href="/static/docs/tcg_privacy_policy.pdf"><a><span onClick={props.onToggleMenu}>Privacy Policy</span></a></Link></li>
+                <li><Link href="/static/docs/tcg_complaints_policy.pdf"><a><span onClick={props.onToggleMenu}>Complaints Policy</span></a></Link></li>
+                <li><Link href="//static/docs/tcg_equality_diversity_policy.pdf"><a><span onClick={props.onToggleMenu}>Equality &amp; Diversity Policy</span></a></Link></li>
             </ul>
         </div>
         <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
