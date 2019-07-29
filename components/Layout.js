eeefccae415965873a1b3dff6df367f635e1714f
Crispin Read <crispin.read@gmail.com>
2019-07-29 10:40:45 +0100

    close links

--- components/Layout.js
+++ components/Layout.js
@@ -43,20 +43,20 @@ class Layout extends React.Component {
                     <link href="/static/css/skel.css" rel="stylesheet" />
                     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css?family=Poppins|Source+Sans+Pro:300,300i,600,600i" rel="stylesheet" />
-                    <link rel="/static/icons/apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-                    <link rel="/static/icons/apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-                    <link rel="/static/icons/apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-                    <link rel="/static/icons/apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-                    <link rel="/static/icons/apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-                    <link rel="/static/icons/apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-                    <link rel="/static/icons/apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-                    <link rel="/static/icons/apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-                    <link rel="/static/icons/apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-                    <link rel="icon" type="image/png" sizes="192x192"  href="/static/icons/android-icon-192x192.png">
-                    <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png">
-                    <link rel="icon" type="image/png" sizes="96x96" href="/static/icons/favicon-96x96.png">
-                    <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png">
-                    <link rel="manifest" href="/manifest.json">
+                    <link rel="/static/icons/apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+                    <link rel="/static/icons/apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+                    <link rel="/static/icons/apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+                    <link rel="/static/icons/apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+                    <link rel="/static/icons/apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+                    <link rel="/static/icons/apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+                    <link rel="/static/icons/apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+                    <link rel="/static/icons/apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+                    <link rel="/static/icons/apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+                    <link rel="icon" type="image/png" sizes="192x192"  href="/static/icons/android-icon-192x192.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="96x96" href="/static/icons/favicon-96x96.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+                    <link rel="manifest" href="/manifest.json" />
                     <meta name="msapplication-TileColor" content="#ffffff" />
                     <meta name="msapplication-TileImage" content="/static/icons/ms-icon-144x144.png" />
                     <meta name="theme-color" content="#ffffff" />
