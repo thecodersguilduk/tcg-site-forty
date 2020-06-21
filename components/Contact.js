0fb6f20450d60cb2f8b45045e79c4eb1b6dc3ccb
Crispin Read <crispin.read@gmail.com>
2020-06-21 08:42:42 +0100

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -2,7 +2,7 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form method="post" action="https://formspree.io/hello@thecodersguild.org.uk" >
+            <form action="https://formspree.io/xjvaoazd" method="POST">
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
                         <input type="text" name="name" id="name" />
